c45a90737a4c2eba3c4b6444be09ae87391ffe40
satyanarayan453 <74782403+satyanarayan453@users.noreply.github.com>
2022-08-08 12:38:14 +0530

    adding environment variable to keep secret of the internet

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.use(session({
-    secret: "This is our little secret",
+    secret: process.env.ANOTHER_SECRET,
     resave: false,
     saveUninitialized: false
 }));
